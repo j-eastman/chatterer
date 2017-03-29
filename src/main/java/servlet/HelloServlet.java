6d0b4d875187855e09fcc783456eb5b089c4b11f
Jack Eastman <jeastman@mscs.mu.edu>
2017-03-29 18:44:45 -0500

    new

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
         name = "MyServlet",
-        urlPatterns = {"/post"}
+        urlPatterns = {"/POST"}
     )
 public class HelloServlet extends HttpServlet {
 
