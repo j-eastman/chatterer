b2af0f0964b6e23961de4f2d265edf7ec9aa08fd
Jack Eastman <jeastman@mscs.mu.edu>
2017-04-03 17:53:27 -0500

    DB fix

--- src/main/java/servlet/KikMsgServlet.java
+++ src/main/java/servlet/KikMsgServlet.java
@@ -13,54 +13,53 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/kikmsg")
-public class KikMsgServlet extends HttpServlet{
-    /**
+public class KikMsgServlet extends HttpServlet {
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+
 	@Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        ServletOutputStream out = resp.getOutputStream();
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		ServletOutputStream out = resp.getOutputStream();
 		out.print("HTTP/1.1 200 \r\n"); // Version & status code
 		out.print("Content-Type: text/plain\r\n"); // The type of data
 		out.print("Connection: close\r\n"); // Will close stream
 		out.print("\r\n"); // End of headers
 		out.println("responded");
-        out.println("You sent a get message");
-        out.flush();
-        out.close();
-    }
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-    	PrintWriter out = resp.getWriter();
-    	BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));
-         String json = "";
-         String line;
-         while ((line = br.readLine()) != null && !line.equals("")) {
-             json += line;
-             System.out.println(json);
-         }
-         if (!json.contains("type")){
-        json = json.substring(1, json.length()-1);
-        JsonMessage jsonMes = new JsonMessage(json);
+		out.println("You sent a get message");
+		out.flush();
+		out.close();
+	}
 
-        System.out.println(jsonMes.get("body") + jsonMes.get("username"));
-        MsgHandler mh = new MsgHandler();
-        mh.postMsg(jsonMes);
-        //String response = mh.getResponse(jsonMes);
-		out.print("HTTP/1.1 200 \r\n"); // Version & status code
-		out.print("Content-Type: text/plain\r\n"); // The type of data
-		out.print("Connection: close\r\n"); // Will close stream
-		out.print("\r\n"); // End of headers
-		out.print("<start>");
-		out.print(mh.getRandomReply(jsonMes));
-		
-		out.print("<end>");
-		//System.out.println("URL: "+ response);
-        out.flush();
-        out.close();
-         }
-    }
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		BufferedReader br = new BufferedReader(new InputStreamReader(req.getInputStream()));
+		String json = "";
+		String line;
+		while ((line = br.readLine()) != null && !line.equals("")) {
+			json += line;
+		}
+		if (!json.contains("type")) {
+			json = json.substring(1, json.length() - 1);
+			JsonMessage jsonMes = new JsonMessage(json);
+
+			System.out.println(jsonMes.get("body") + jsonMes.get("username"));
+			MsgHandler mh = new MsgHandler();
+			mh.postMsg(jsonMes);
+			// String response = mh.getResponse(jsonMes);
+			out.print("HTTP/1.1 200 \r\n"); // Version & status code
+			out.print("Content-Type: text/plain\r\n"); // The type of data
+			out.print("Connection: close\r\n"); // Will close stream
+			out.print("\r\n"); // End of headers
+			out.print("<start>");
+			out.print(mh.getRandomReply(jsonMes));
+
+			out.print("<end>");
+			// System.out.println("URL: "+ response);
+			out.flush();
+			out.close();
+		}
+	}
 }
