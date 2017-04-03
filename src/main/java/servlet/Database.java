0b80fd6f73dedac9a8908d52591b39a0d05c349a
Jack Eastman <jeastman@mscs.mu.edu>
2017-04-03 14:58:22 -0500

    DB fix

--- src/main/java/servlet/Database.java
+++ src/main/java/servlet/Database.java
@@ -103,6 +103,7 @@ public class Database {
 	}
 
 	private int getIndex(String s) {
+		System.out.println("getIndex: " + s);
 		s = s.toLowerCase();
 		if (s.equals("")) {
 			if (s.charAt(0) - 97 < 0 || s.charAt(0) - 97 > 26) {
