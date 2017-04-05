f2fb9dcd3d804661ba64f0a65ef15d0b1ca96d51
Jack Eastman <jeastman@mscs.mu.edu>
2017-04-05 17:14:52 -0500

    Trying to fix db error...

--- src/main/java/servlet/Database.java
+++ src/main/java/servlet/Database.java
@@ -74,8 +74,8 @@ public class Database {
 	public void updateEntry(String entry, int table,String newVal) {
 		// UPDATE table SET responses = 'newVal' WHERE word = entry;
 		//System.out.println(form(newVal));
-		System.out.printf("Updating entry %s with responses: '%s' into table: %s\n", entry, newVal,tables[table]);
-		String sql = String.format("UPDATE %s SET responses = %s WHERE word='%s';", tables[table], newVal, entry);
+		System.out.printf("Updating entry %s with resstr: '%s' into table: %s\n", entry, newVal,tables[table]);
+		String sql = String.format("UPDATE %s SET resstr = %s WHERE word='%s';", tables[table], newVal, entry);
 		try {
 			Statement stmt = conn.createStatement();
 			stmt.executeUpdate(sql);
