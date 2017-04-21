861bc4b487265416bec94521b22120752b8fd6dd
Jack Eastman <jeastman@mscs.mu.edu>
2017-04-21 17:06:12 -0500

    fix

--- src/main/java/tools/JsonMessage.java
+++ src/main/java/tools/JsonMessage.java
@@ -6,7 +6,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class JsonMessage {
-	JSONObject json;
+	public JSONObject json;
 	String message;
 	String username;
 	public JsonMessage(String js) throws IOException{
