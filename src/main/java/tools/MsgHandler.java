17c41f2c23ece274fca0cb87e0ad9546fd3b5117
Jack Eastman <jeastman@mscs.mu.edu>
2017-04-21 13:15:02 -0500

    Java fix

--- src/main/java/tools/MsgHandler.java
+++ src/main/java/tools/MsgHandler.java
@@ -33,7 +33,7 @@ public class MsgHandler {
 		Random r = new Random();
 		String respStr = db.getResStr(msg);
 		System.out.println("RespStr: " + respStr);
-		if (respStr.equals("<brk>") || respStr.equals("") || respStr.equals("null")){
+		if (respStr == null || respStr.equals("<brk>") || respStr.equals("") || respStr.equals("null")){
 			System.out.println("No response found, random response.");
 			ArrayList<String> all = new ArrayList<String>();
 			all = db.all;
