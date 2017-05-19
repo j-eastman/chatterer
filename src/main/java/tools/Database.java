bd4c2755b12224f60901dba7cbf1cf811db03167
Jack Eastman <jeastman@mscs.mu.edu>
2017-05-19 12:33:42 -0500

    fix

--- src/main/java/tools/Database.java
+++ src/main/java/tools/Database.java
@@ -2,7 +2,6 @@ package tools;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.sql.Array;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -65,7 +64,13 @@ public class Database {
 
 	private Connection getConnection() throws URISyntaxException, SQLException {
 		URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
+		System.out.println("DATABASEURL: " + System.getenv("DATABASE_URL"));
+		System.out.println("USERNAME: " + dbUri.getUserInfo().split(":")[0]);
+		System.out.println("PASSWORD: " + dbUri.getUserInfo().split(":")[1]);
+		System.out.println("HOST: " + dbUri.getHost());
+		System.out.println("PORT: " + dbUri.getPort());
+		System.out.println("Path: " + dbUri.getPath());
+		
 		String username = dbUri.getUserInfo().split(":")[0];
 		String password = dbUri.getUserInfo().split(":")[1];
 		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
